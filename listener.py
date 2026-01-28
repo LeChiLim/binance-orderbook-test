bfadad0f9c4fde5da5112af0c175d4173e9faac6
LeChiLim <limlechi@gmail.com>
2026-01-28 16:10:00 +0800

    no binance libraries, websocket listner to event updates

--- listener.py
+++ listener.py
@@ -5,19 +5,32 @@
 import asyncio
 import websockets
 import json
-from binance_sdk_spot.spot import Spot
+#from binance_sdk_spot.spot import Spot, ConfigurationWebSocketStreams, Spot, SPOT_WS_STREAMS_PROD_URL
+import logging
 
 #== CONFIGURATION ==#
-TRADE_SYMBOL = "NEARUSDT"
-URL = f"wss://fstream.binance.com/ws/{TRADE_SYMBOL}@depth@100ms"
+TRADE_SYMBOL = "nearusdt"
+WS_URL = f"wss://fstream.binance.com/ws/{TRADE_SYMBOL}@depth10@100ms"
 
+logging.basicConfig(level=logging.INFO)
 
-client = Spot()
+async def listen_to_depth():
+    async with websockets.connect(WS_URL) as ws:
+        logging.info(f"Connected to {WS_URL}")
+        
+        while True:
+            try:
+                message = await ws.recv()
+                data = json.loads(message)
+                
+                if 'e' in data and data['e'] == 'depthUpdate':
+                    logging.info(f"Depth Update:\n{json.dumps(data, indent=2)}")
+                    # Apply to your local book here
+                else:
+                    logging.info(f"Other message: {data}")
+                    
+            except Exception as e:
+                logging.error(f"Error in loop: {e}")
+                break
 
-response = client.rest_api.depth(
-   symbol=TRADE_SYMBOL,
-   limit=5
-)
-print(response.data())
-
-
+asyncio.run(listen_to_depth())
