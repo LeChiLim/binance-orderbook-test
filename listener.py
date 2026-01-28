35737852de5ee4b2cc0f4414ca36ecc63953c757
LeChiLim <limlechi@gmail.com>
2026-01-28 15:43:42 +0800

    retriever orderbook snapshot

--- listener.py
+++ listener.py
@@ -8,27 +8,16 @@ import json
 from binance_sdk_spot.spot import Spot
 
 #== CONFIGURATION ==#
-URL = "wss://fstream.binance.com/ws/<symbol>@depth@100ms"
+TRADE_SYMBOL = "NEARUSDT"
+URL = f"wss://fstream.binance.com/ws/{TRADE_SYMBOL}@depth@100ms"
 
-def get_all_symbols():
-    
-    client = Spot()
 
-    response = client.rest_api.exchange_info()
-    data = response.data()
+client = Spot()
 
-    all_symbols = [symbol_info['symbol'] for symbol_info in data.symbols]
-    #usdt_symbols = [symbol for symbol in data.symbols if symbol.quote_asset == "USDT"]
-    #usdt_trading_symbols = [symbol for symbol in usdt_symbols if symbol.status == "TRADING"]
-    return all_symbols
+response = client.rest_api.depth(
+   symbol=TRADE_SYMBOL,
+   limit=5
+)
+print(response.data())
 
-def get_url(symbol):
-    #all_trading_symbols = get_all_symbols()
-    #if symbol not in all_trading_symbols:
-    #    raise ValueError("Symbol not found in trading symbols")
-    return URL.replace("<symbol>", symbol)
-
-#print(get_all_symbols())
-print("=========================")
-print(get_url("BTCUSDT"))
 
